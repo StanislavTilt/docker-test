9121e5632fec421437d492d54677b70aee4b8ed3
Stanislav Tilt <stasispuha@gmail.com>
2024-03-26 23:39:44 +0200

    commit

---
+++ src/app/Http/Controllers/Api/V1/TestController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+/**
+ * @group Test
+ */
+class TestController extends Controller
+{
+    /**
+     * test api doc
+     */
+    public function __invoke()
+    {
+        return;
+    }
+}
